91828d82ac133051c06ab1e462d915732c5c9250
abhijitWebDev <110275876+abhijitWebDev@users.noreply.github.com>
2024-08-31 16:05:44 +0530

    performed addition operation

--- hello.cpp
+++ hello.cpp
@@ -3,5 +3,17 @@
 using namespace std;
 
 int main(){
-    cout<<"Hello chai from hitesh";
+    cout<<" Hello chai from hitesh "<< endl;
+
+    int a = 20;
+    int b = 30;
+
+    int c = a + b;
+
+    cout<<" \n The result is " << c << endl;
+
+    string d = "hello";
+
+    cout<< "\n the variable d is a " << d << endl;
+    return 0;
 }
