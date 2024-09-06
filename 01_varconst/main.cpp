c904f82991666a8a07eb4bad9522eb6bd993d283
abhijitWebDev <abhijitmone2@gmail.com>
2024-09-06 21:07:53 +0530

    vadriable and constant initial learning

---
+++ 01_varconst/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int score;
+    score = 110;
+
+    const int uid = 12345;
+
+    int balance = 500;
+
+    // uid = 123445;
+    cout<< "Welcome to chai with cpp 1"<<endl;
+    cout<< "Welcome to chai with cpp 2"<<endl;
+    cout<< "Welcome to chai with cpp 3"<<endl;
+    return 0;
+}
